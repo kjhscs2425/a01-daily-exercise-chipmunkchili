05e9385e401f3f182d4b2562f7e2a57c7ad7ca21
Stella Talcott <stalcott2027@kehillahstudent.org>
2024-09-16 20:37:54 -0700

    corrected final version

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import turtle
 
 side = 100
-for i in range(6):
+def draw_triangles():
     for i in range(3):
-        turtle.forward(side)
+        for i in range(3):
+            turtle.forward(side)
+            turtle.right(120)
         turtle.right(120)
-    turtle.right(120)
+
+
+draw_triangles()
+turtle.right(180)
+draw_triangles()
 
 
 turtle.exitonclick()
